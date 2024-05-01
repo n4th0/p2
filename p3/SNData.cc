03f55faef2517aed07233c9f6114f3821364dc11
n4th0 <nathanmoyses@gmail.com>
2024-05-01 20:52:07 +0200

    Add coments

--- p3/SNData.cc
+++ p3/SNData.cc
@@ -6,6 +6,18 @@ using namespace std;
 
 std::vector<struct SocialNetworkData> SNData::sns;
 
+/*
+ * Añade al final del vector sns un nuevo registro con los datos de la nueva red social. 
+ * Si ya existía una red con ese nombre, se lanza la excepción estándar invalid_argument pasándole 
+ * como parámetro el nombre de la red.
+ *
+ * Los valores avgR y avgM deben ser mayores que 0 y menores que 1, en caso contrario se debe lanzar
+ * la excepción invalid_argument pasándole como argumento el valor incorrecto convertido a cadena
+ * con to_string, por ejemplo con to_string(avgM)
+ *
+ *
+ * */
+
 void SNData::newSocialNetwork(string name, double avgR, double avgM){
     struct SocialNetworkData snd;
 
@@ -13,11 +25,11 @@ void SNData::newSocialNetwork(string name, double avgR, double avgM){
         throw invalid_argument(name);
     }
 
-    if (avgR<0 || avgR>1) {
+    if ( avgR<0 || avgR>1 ) {
         throw invalid_argument(to_string(avgR));
     }
 
-    if (avgM<0 || avgM>1) {
+    if ( avgM<0 || avgM>1 ) {
         throw invalid_argument(to_string(avgM));
     }
 
@@ -29,37 +41,47 @@ void SNData::newSocialNetwork(string name, double avgR, double avgM){
 
 }
 
+/*
+ * Busca en el vector sns una red social llamada name, y devuelve true
+ * si la encuentra, y false en otro caso.
+ * */
 bool SNData::checkSN(std::string name){
     bool found = false;
-    // std::cout << "llego aqui fn2 1"<< std::endl;
+
     for(unsigned int i = 0; i<sns.size(); i++){
         if (sns[i].name == name) {
-            // std::cout << "llego aqui fn2 2"<< std::endl;
             found = true;
         }
     }
-    // std::cout << "llego aqui fn2 3"<< std::endl;
-    return found;
 
+    return found;
 }
 
+/*
+ * Devuelve la valoración promedio de la red social llamada
+ * name, o 0.0 si no la encuentra.
+ * */
 double SNData::getAvgRating(std::string name){
     for(unsigned int i = 0; i<sns.size(); i++){
         if (sns[i].name == name) {
             return sns[i].averageRating;
         }
     }
-    return 0.0;
 
+    return 0.0;
 }
 
+/*
+ * Devuelve la monetización media de la red social llamada
+ * name, o 0.0 si no la encuentra.
+ * */
 double SNData::getAvgMonetizing(std::string name){
     for(unsigned int i = 0; i<sns.size(); i++){
         if (sns[i].name == name) {
             return sns[i].averageMonetizing;
         }
     }
-    return 0.0;
 
+    return 0.0;
 }
 
--- p3/SNFollowers.cc
+++ p3/SNFollowers.cc
@@ -6,6 +6,13 @@
 #include <string>
 using namespace std;
 
+/*
+ * Constructor que crea un nuevo objeto con los datos pasados por parámetros, y con un valor inicial del dinero 
+ * (money) de 0. Si la red social no existe, se lanzará la excepción EXCEPTION_UNKNOWN_SN; 
+ * si el número inicial de seguidores es menor que 0 se lanzará la excepción estándar 
+ * invalid_argument pasándole como parámetro dicho número (convertido a cadena con la función to_string, 
+ * ya que el parámetro de esa excepción debe ser una cadena).
+ * */
 SNFollowers::SNFollowers(std::string name, int initialFollowers){
     if(!SNData::checkSN(name)){
         throw EXCEPTION_UNKNOWN_SN;
@@ -17,18 +24,36 @@ SNFollowers::SNFollowers(std::string name, int initialFollowers){
 
     this->name = name;
     this->numFollowers = initialFollowers;
-    this->money = 0.0;
+    this->money = 0;
 }
 
+/* 
+ * Suma al número de seguidores el valor pasado por parámetro (que podría ser negativo si el influencer 
+ * pierde seguidores). Después de la suma, si el valor del atributo numFollowers es negativo se debe poner a 
+ * 0 (el número de seguidores no puede ser negativo, como mínimo debe ser 0).
+ * */
 void SNFollowers::addFollowers(int nf){
     this->numFollowers = this->numFollowers + nf;
 
     if (this->numFollowers <0) {
         this->numFollowers = 0;
     }
-
 }
 
+/*
+ * Método que recalcula el número de seguidores (y la monetización
+ * obtenida) en función de la valoración del evento (rating), que se pasa como parámetro, y la valora-
+ * ción promedio de la red social. Si rating es negativo debe lanzarse la excepción invalid_argument
+ * con rating (convertido a cadena con to_string). El cálculo se realiza de la siguiente manera:
+ *
+ * • Si la relación entre la valoración del evento y la de la red es mayor que 0,8, el número de
+ * seguidores se incrementará en una cantidad que será el valor de dicha relación multiplicado
+ * por el número actual de seguidores (tomando la parte entera de dicha operación). 
+ *
+ * • Si la relación es menor o igual que 0,8 se perderán seguidores y no se obtendrá dinero. La
+ * cantidad de seguidores perdida será la resta de 0,9 menos la relación (p.ej. si la relación es
+ * 0,7, la resta sería 0,2) multiplicada por el número de seguidores (tomando la parte entera).  
+ * */
 void SNFollowers::addEvent(double rating){
     double relat;
     int followersAdded;
@@ -39,8 +64,7 @@ void SNFollowers::addEvent(double rating){
 
     relat = rating/SNData::getAvgRating(this->name);
 
-    if( relat > 0.8){ // 0.8 debería ser constante
-        
+    if(relat > 0.8){ 
         followersAdded = this->numFollowers*relat;
         this->addFollowers(followersAdded);
 
@@ -48,20 +72,26 @@ void SNFollowers::addEvent(double rating){
 
     }else {
         this->addFollowers((int) this->numFollowers*(0.9 - relat));
-    
     }
-
-
 }
 
+/* 
+ * Calcula la monetización que corresponde a la agencia, multiplicando la comisión pasada como 
+ * parámetro por la cantidad de dinero obtenida hasta el momento, y la devuelve. Antes de finalizar 
+ * pone la cantidad de dinero a 0 porque la agencia va a cobrar dicha comisión inmediatamente, y
+ * debe resetear esa cantidad a 0 para que la próxima vez que se recolecte dinero no se cobre comisión 
+ * sobre el dinero anterior por el que ya se ha cobrado (los influencers se darían cuenta). 
+ * El valor de commission debe ser mayor que 0 y menor que 1, en caso contrario se lanzará la excepción 
+ * invalid_argument (convirtiendo a cadena la dicho valor) sin hacer nada más, como en otros métodos.
+ * */
 double SNFollowers::collectCommission(double commission){
     double a;
 
     if (commission<0 || commission>1) {
         throw invalid_argument(to_string(commission));
     }
-    
-    a = commission * SNFollowers::money;
+
+    a = commission * this->money;
     this->money = 0;
     return a;
 }
